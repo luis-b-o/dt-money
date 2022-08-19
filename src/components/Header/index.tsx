a65d768f6acc51b7bd32f6e295e845896ea3befb
Luis Oliveira <luis.oliveira@startse.com>
2022-08-19 10:31:44 -0300

    feat: accessibility content manager

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,13 +4,27 @@ import {
   HeaderContent,
   NewTransactionButton,
 } from "@components/Header/styles";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={Logo} alt="" />
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
